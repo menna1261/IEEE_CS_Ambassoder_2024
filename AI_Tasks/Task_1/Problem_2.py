c4ac31d45f6908d7accdb158a6f31fc3d4557c2f
Menna Jaheen <118180052+menna1261@users.noreply.github.com>
2024-08-12 15:33:28 +0300

    Update Problem_2.py

--- AI_Tasks/Task_1/Problem_2.py
+++ AI_Tasks/Task_1/Problem_2.py
@@ -1,36 +1,36 @@
-Day = int(input("Day: "))
-Month = int(input("Month: "))
-Year = int(input("Year: "))
+day = int(input("Day: "))
+month = int(input("Month: "))
+year = int(input("Year: "))
 
 def is_leap_year(year):
     return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
-if Month == 2:
-    if is_leap_year(Year) and Day == 29:
-        Day = 1
-        Month += 1
-    elif not is_leap_year(Year) and Day == 28:
-        Day = 1
-        Month += 1
+if month == 2:
+    if is_leap_year(year) and day == 29:
+        day = 1
+        month += 1
+    elif not is_leap_year(year) and day == 28:
+        day = 1
+        month += 1
     else:
-        Day += 1
+        day += 1
 
-elif (Month == 1 or Month == 3 or Month == 5 or Month == 7 or Month == 8 or Month == 10 or Month == 12):
-    if Day == 31:
-        Day = 1
-        if Month == 12:
-            Month = 1
-            Year += 1
+elif (month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12):
+    if day == 31:
+        day = 1
+        if month == 12:
+            month = 1
+            year += 1
         else:
-            Month += 1
+            month += 1
     else:
-        Day += 1
+        day += 1
 
-elif (Month == 4 or Month == 6 or Month == 9 or Month == 11):
-    if Day == 30:
-        Day = 1
-        Month += 1
+elif (month == 4 or month == 6 or month == 9 or month == 11):
+    if day == 30:
+        day = 1
+        month += 1
     else:
-        Day += 1
+        day += 1
 
-print(f"Day: {Day} Month: {Month} Year: {Year}")
+print(f"Day: {day} Month: {month} Year: {year}")

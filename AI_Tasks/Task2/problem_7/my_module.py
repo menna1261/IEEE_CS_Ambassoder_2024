25156d59432485eeea3ea150d5944927479abf87
Menna Jaheen <118180052+menna1261@users.noreply.github.com>
2024-08-17 20:08:12 +0300

    Create my_module.py

---
+++ AI_Tasks/Task2/problem_7/my_module.py
@@ -0,0 +1,13 @@
+#my_module.py:
+def add(num1, num2):
+    return num1+num2
+    
+def sub(num1,num2):
+    return num1-num2
+
+def multiply(num1,num2):
+    return num1*num2
+    
+def divide(num1,num2):
+    return num1/num2
+    

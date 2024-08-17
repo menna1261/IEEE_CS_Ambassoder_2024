9c056f900165821631396fdfd2ec45a53cfa956f
Menna Jaheen <118180052+menna1261@users.noreply.github.com>
2024-08-17 20:11:02 +0300

    Create main.py

---
+++ AI_Tasks/Task2/problem_7/main.py
@@ -0,0 +1,13 @@
+import my_module as mj
+
+result = mj.add(5,6)
+print(result)
+
+result = mj.sub(5,6)
+print(result)
+
+result = mj.multiply(5,6)
+print(result)
+
+result = mj.divide(5,6)
+print(result)
